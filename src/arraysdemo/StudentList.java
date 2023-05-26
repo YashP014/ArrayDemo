73dcadbd6160bb16971631f5079ce5c198730cdd
Yash Patel <payashbe@sheridancollege.ca>
2023-05-26 13:24:33 -0400

    added new class

--- src/arraysdemo/StudentList.java
+++ src/arraysdemo/StudentList.java
@@ -6,7 +6,7 @@ public class StudentList {
     public static void main(String[] args){
         Student s1=new Student("s1","ronak");
         Student[] studentList= new Student[3];
-        
+  
         studentList[0]=s1;
         studentList[1]=new Student("s2","abc");
         studentList[2]=new Student("s3","xyz");
